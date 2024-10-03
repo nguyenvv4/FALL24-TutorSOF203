8bf2a912b3350addd23addf5d523b94318849eab
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2024-10-03 12:40:40 +0700

    update

---
+++ src/main/java/com/example/fall24_tutor/DbConnection.java
@@ -0,0 +1,55 @@
+package com.example.fall24_tutor;
+
+import java.sql.*;
+
+/**
+ *
+ * @author nguyenvv
+ */
+public class DbConnection {
+
+    public static final String HOSTNAME = "localhost";
+    public static final String PORT = "1433";
+    public static final String DBNAME = "FALL24SOF302";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "Password.1";
+    /*
+    host name: localhost
+    port mặc định là 1433
+    DBNAME: tên database của mình
+
+    */
+
+    /**
+     * Get connection to MSSQL Server
+     *
+     * @return Connection
+     */
+    public static Connection getConnection() {
+
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:sqlserver://" + HOSTNAME + ":" + PORT + ";"
+                + "databaseName=" + DBNAME + ";encrypt=true;trustservercertificate=true;";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            return DriverManager.getConnection(connectionUrl, USERNAME, PASSWORD);
+        } // Handle any errors that may have occurred.
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace(System.out);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Connection conn = getConnection();
+        DatabaseMetaData dbmt = conn.getMetaData();
+        System.out.println(dbmt.getDriverName());
+        System.out.println(dbmt.getDatabaseProductName());
+        System.out.println(dbmt.getDatabaseProductVersion());
+    }
+
+    /*
+
+     */
+
+}
